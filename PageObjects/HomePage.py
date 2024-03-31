04aedeb885c179edceb7f6cad361e8980e22cdeb
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2024-03-31 11:06:52 +0300

    search_book func

--- PageObjects/HomePage.py
+++ PageObjects/HomePage.py
@@ -1,9 +1,18 @@
+from Utilities.Common import Common
+
+
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
+        self.common = Common(self.driver, './Elements/home_page.ini')
 
     def open(self):
         self.driver.get('https://www.kitapyurdu.com/')
 
     def get_title(self):
         return self.driver.title
+
+    def search_book(self, text):
+        element = self.common.get_element('Search')
+        element.send_keys(text)
+        return element.get_attribute("value") == text
