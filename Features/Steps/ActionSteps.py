8c110ee250e42f28118c3b755bacec26ad5ac2b4
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2026-08-20 13:51:02 +0300

    I enter step implementation

--- Features/Steps/ActionSteps.py
+++ Features/Steps/ActionSteps.py
@@ -1,7 +1,11 @@
-from behave import given, when, then, step
-from Utilities.WebDriver import get_driver
+from Utilities.StepHelper import step
 
 
 @step('I click on {button_name} button')
 def step_impl(context, button_name):
     assert context.common.element_click(button_name) is None, f'Failed to click on button: {button_name}'
+
+
+@step('I enter "{text}" into {field_name} field')
+def step_impl(context, text, field_name):
+    assert context.common.enter_text(field_name, text) is None, f'Failed to enter text into field: {field_name}'
