dd2cdfa0c45967cdd8f258081e5649c4e2f5ac6b
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2026-08-20 13:52:37 +0300

    I see text step implementation added

--- Features/Steps/ValidationSteps.py
+++ Features/Steps/ValidationSteps.py
@@ -1,1 +1,8 @@
-from behave import given, when, then, step
+from Utilities.StepHelper import step
+
+
+@step('I see "{text}" as {field_name} field value')
+def step_impl(context, text, field_name):
+    element = context.common.get_element(field_name)
+    actual_value = element.get_attribute("value")
+    assert actual_value == text, f'Expected value "{text}" but got "{actual_value}" for field: {field_name}'
