6d5bcd39ba498cff5711fe8103e9a2df7f981b65
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2026-08-21 13:53:54 +0300

    Validation steps update

--- Features/Steps/ValidationSteps.py
+++ Features/Steps/ValidationSteps.py
@@ -1,12 +1,12 @@
 from Utilities.StepHelper import step
 
 
-
 @step('I see "{text}" as {field_name} field value')
 def step_impl(context, text, field_name):
     element = context.common.get_element(field_name)
     actual_value = element.get_attribute("value")
     actual_visible_text = context.elementHelper.get_element_visible_text(element)
+    actual_value = actual_value if actual_value is not None else actual_visible_text
     assert actual_value == text or actual_visible_text == text, f'Expected value "{text}" but got "{actual_value}" for field: {field_name}'
 
 
